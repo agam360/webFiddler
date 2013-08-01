fff837e373b102bf1ec647dca4dc5f0235d8236e
Agam <agam360@gmail.com>
2013-08-01 22:06:03 +0300

    Inital commit of the project - basic stracture done

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var ehtml = ace.edit("editorHTML").getSession().setMode("ace/mode/html");
+var ecss = ace.edit("editorCSS").getSession().setMode("ace/mode/css");
+var ejs = ace.edit("editorJS").getSession().setMode("ace/mode/javascript");
+
